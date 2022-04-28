1dc2abf57504afad41d1797b3021e4019e573abf
ISAI ARON AGUILERA CASTILLO <isaiaron@hotmail.com>
2022-04-28 11:39:12 -0600

    Agregar codigo para la caracteristicas del servidor en la bisqueda

--- Web_Server/Servidor.js
+++ Web_Server/Servidor.js
@@ -14,6 +14,15 @@ if (filePath=='./'){
 }
 
 
+var extname = String (path.extname(filePath)).toLowerCase ();
+var contentType = 'text/html'
+var mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    'css' : 'text/css',
+    '.jpg' : 'image/jpg',
+};
+
 
 
 })
